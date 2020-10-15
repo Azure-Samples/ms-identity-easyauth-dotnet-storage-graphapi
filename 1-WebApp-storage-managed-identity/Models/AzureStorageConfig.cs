b9c66c3929b3c74fd0ecf182133330583f073d7e
Ryan Wike <ryanwi@microsoft.com>
2020-10-15 12:47:48 -0700

    initial checkin

---
+++ 1-WebApp-storage-managed-identity/Models/AzureStorageConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_EasyAuth_DotNet.Models
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName { get; set; }
+        public string ContainerName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
