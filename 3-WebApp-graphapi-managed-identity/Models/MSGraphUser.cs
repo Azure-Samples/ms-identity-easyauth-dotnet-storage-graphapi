e818fbaa1d752e66006700f76639de2baa58e16a
Ryan Wike <>
2024-02-17 13:33:17 -0800

    Updated for net 8

---
+++ 3-WebApp-graphapi-managed-identity/Models/MSGraphUser.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp_EasyAuth_DotNet.Models
+{
+    public class MSGraphUser
+    {
+        public string displayName { get; set; }
+        public string jobTitle { get; set; }
+        public string mail { get; set; }
+        public string userPrincipalName { get; set; }
+        public string id { get; set; }
+    }
+}
--- 3-WebApp-graphapi-managed-identity/Program.cs
+++ 3-WebApp-graphapi-managed-identity/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,11 +23,16 @@ builder.Services.AddRazorPages()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    IdentityModelEventSource.ShowPII = true;
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
